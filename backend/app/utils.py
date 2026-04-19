ee9cc97abb14eb1dd9ee68a4d026ce7d00c52dfe
Ramya07-cs <rakshanaramesh2020@gmail.com>
2026-04-20 00:30:03 +0530

    fix: enable dynamic repository quality scoring.Closes #6

--- backend/app/utils.py
+++ backend/app/utils.py
@@ -1,6 +1,7 @@
 from typing import Tuple
 from app.schemas import GitHubRepo,LanguageBreakdown,RepoWithScore
 from collections import Counter
+from datetime import datetime,timedelta,timezone
 
 def calculate_stars_and_forks(repos : list[GitHubRepo]) -> Tuple[int,int]:
     """Calculates the total stars and forks for original works."""
@@ -30,15 +31,25 @@ def calculate_language_breakdown(repos : list[GitHubRepo]) -> list[LanguageBreak
 
     return sorted(breakdown, key=lambda l: l.percentage, reverse=True) # Sort by percentage descending
     
+def is_recent(date_str : str) -> bool:
+    date = datetime.fromisoformat(date_str.replace("Z", "+00:00"))  #The "Z" in the timestamp stands for UTC,so we parse it as such
+    
+    now = datetime.now(timezone.utc)   #gives the current time in utc
+
+    difference = now - date
+    return difference < timedelta(days = 30)  
+
 def calculate_repo_quality_score(repos : list[GitHubRepo]) -> list[RepoWithScore]:
     """ Calculate repo_quality_score for each repo"""
     repos_with_scores = []
 
     for repo in repos:
         score = 0
-        score += min(repo.stars * 5, 25) # 2 points per star,capped at 25
-        score += min(repo.forks * 5,25)
+    
+        score += min(repo.stars * 5, 20) # 2 points per star,capped at 25
+        score += min(repo.forks * 5,20)
         score += 10 if repo.description else 0
+        score += 10 if is_recent(repo.updated_at) else 0
         score += min(len(repo.topics) * 2, 20) # 2 points per topic, capped at 20
         score += 10 if repo.license else 0
         score = score + 10 if not repo.fork else score*0.2
